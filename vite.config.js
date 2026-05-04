a1a590113ee93db6513c3fddb386e5e72be596bb
bacanapps <colin@cpantin.com>
2026-05-04 18:42:46 -0300

    Fix base path format in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
 export default defineConfig({
-  base: '/send-link-to-meta-quest',
+  base: '/send-link-to-meta-quest/',
   plugins: [react()],
   resolve: {
     alias: {
